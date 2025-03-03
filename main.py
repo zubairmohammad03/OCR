21a7da350c10a04dead44b7f254d686282c90a21
Mohd. Zubair <154042871+zubairmohammad03@users.noreply.github.com>
2025-03-03 14:51:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import base64
 import os
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
 
 app = Flask(__name__)
 
